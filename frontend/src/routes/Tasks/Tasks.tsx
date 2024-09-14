2e6baeee75e24cd46abe1e100ba7b6d541250218
Zakkkk <zak.g.venter@gmail.com>
2024-09-14 22:45:31 +1000

    text changes

--- frontend/src/routes/Tasks/Tasks.tsx
+++ frontend/src/routes/Tasks/Tasks.tsx
@@ -4,21 +4,30 @@ const Tasks: React.FC = () => {
   return (
     <div className={"p-5"}>
       <h1 className={"text-white text-3xl font-bold"}>View and Edit Tasks</h1>
-      <p>
-        View all your current tasks and add new tasks.
-      </p>
+      <p>View all your current tasks and add new tasks.</p>
 
       <br />
 
       <div className={"flex flex-col gap-5"}>
-        <CardPreview link={"/tasks/create"} title={"Add"} description={"Create a new repeating task."}
-                     icon={"library_add"} />
-        <CardPreview link={"/tasks/edit"} title={"Modify"} description={"Modify or delete an existing task."}
-                     icon={"build"} />
-        <CardPreview link={"/tasks/pool"} title={"Pool"} description={"View the pool of existing tasks."}
-                     icon={"lists"} />
+        <CardPreview
+          link={"/tasks/create"}
+          title={"Add"}
+          description={"Create a new task generator."}
+          icon={"library_add"}
+        />
+        <CardPreview
+          link={"/tasks/edit"}
+          title={"Modify"}
+          description={"Modify or delete an existing task generator."}
+          icon={"build"}
+        />
+        <CardPreview
+          link={"/tasks/pool"}
+          title={"Pool"}
+          description={"View the pool of existing tasks."}
+          icon={"lists"}
+        />
       </div>
-
     </div>
   );
 };
